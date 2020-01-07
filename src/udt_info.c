4be01e27b62d53a8fadf3fdbd0c0b76715827177
ququzone <xueping.yang@gmail.com>
2020-01-07 13:40:05 +0800

    add udt info script flow

--- src/udt_info.c
+++ src/udt_info.c
@@ -2,6 +2,10 @@
 #include "ckb_syscalls.h"
 
 int main(int argc, char* argv[]) {
-  // TODO
+  // TODO steps
+  // 1. UUID in args
+  // 2. metadata in data
+  // 3. create UDT, BALANCE_CELL == total supply
+  // 4. burn/mint UDT, check total supply
   return 0;
 }
