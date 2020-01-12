bcb532b6fe9a27fe7eb996bdb620432ef5af15c6
ququzone <xueping.yang@gmail.com>
2020-01-12 10:28:17 +0800

    add code hash utils

---
+++ deploy/deploper-codehash.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"github.com/ququzone/ckb-sdk-go/crypto/blake2b"
+	"log"
+	"os"
+)
+
+func main() {
+	dataFile, err := os.Open("./udt")
+	if err != nil {
+		log.Fatalf("load data file error: %v", err)
+	}
+	defer dataFile.Close()
+
+	dataInfo, err := dataFile.Stat()
+	if err != nil {
+		log.Fatalf("load data info error: %v", err)
+	}
+
+	data := make([]byte, dataInfo.Size())
+	_, err = dataFile.Read(data)
+	if err != nil {
+		log.Fatalf("read data file error: %v", err)
+	}
+
+	hash, _ := blake2b.Blake256(data)
+
+	fmt.Println(hex.EncodeToString(hash))
+}
